8ebd1249e5c3cbe0f0c7cd389e98acefc648ecde
Alex Gubin <alex_luza@mail.ru>
2020-07-16 23:06:28 +0300

    Change1

--- Classes.py
+++ Classes.py
@@ -1,6 +1,5 @@
-class Animals:
+class Animal:
     place = "Ферма Дядюшки Джо\n"
-    print(place)
     state = None
     sound = None
 
@@ -16,62 +15,54 @@ class Animals:
         self.state = "Употрлебляет пищу"
         return self.state
 
-class Gooses(Animals):
-    name = "Гусь"
-    sound = "Га-Га"
+    def voice(self):
+        return self.sound
 
     def lay_eggs(self):
-        self.state = "Получены гусинные яйца"
+        self.state = "Получены яйца"
         return self.state
-
-class Cows(Animals):
-    name = "Корова"
-    sound = "Му-му"
 
     def get_milk(self):
-        self.state = "Получено коровье молоко"
+        self.state = "Получено молоко"
         return self.state
-
-class Sheeps(Animals):
-    name = "Овца"
-    sound = "Бе-Бе"
 
     def trim(self):
         self.state = "Подстрижен"
         return self.state
 
-class Hens(Animals):
+class Gooses(Animal):
+    name = "Гусь"
+    sound = "Га-Га"
+
+class Cows(Animal):
+    name = "Корова"
+    sound = "Му-му"
+
+class Sheeps(Animal):
+    name = "Овца"
+    sound = "Бе-Бе"
+
+class Hens(Animal):
     name = "Курица"
     sound = "Ку-ка-ре-ку"
 
-    def lay_eggs(self):
-        self.state = "Получены куринные яйца"
-        return self.state
-
-class Goats(Animals):
+class Goats(Animal):
     name = "Коза"
     sound = "Ме-Ме"
 
-    def get_milk(self):
-        self.state = "Получено козье молоко"
-        return self.state
-
-class Ducks(Animals):
+class Ducks(Animal):
     name = "Утка"
     sound = "Кря-Кря"
 
-    def lay_eggs(self):
-        self.state = "Получены гусинные яйца"
-        return self.state
-
 
 '''Объекты'''
+print(Animal.place)
 animals_dict = {}
 
 Goose1 = Gooses("Серый", 5)
 animals_dict[Goose1.name] = Goose1.weight
 print(Goose1.name)
-print(Goose1.sound)
+print(Goose1.voice())
 print(Goose1.walk())
 print(Goose1.eat())
 print(Goose1.lay_eggs())
@@ -80,7 +71,7 @@ print()
 Goose2 = Gooses("Белый", 7)
 animals_dict[Goose2.name] = Goose2.weight
 print(Goose2.name)
-print(Goose2.sound)
+print(Goose2.voice())
 print(Goose2.walk())
 print(Goose2.eat())
 print(Goose2.lay_eggs())
@@ -89,7 +80,7 @@ print()
 Cow1 = Cows("Манька", 400)
 animals_dict[Cow1.name] = Cow1.weight
 print(Cow1.name)
-print(Cow1.sound)
+print(Cow1.voice())
 print(Cow1.walk())
 print(Cow1.eat())
 print(Cow1.get_milk())
@@ -98,7 +89,7 @@ print()
 Sheep1 = Sheeps("Барашек", 65)
 animals_dict[Sheep1.name] = Sheep1.weight
 print(Sheep1.name)
-print(Sheep1.sound)
+print(Sheep1.voice())
 print(Sheep1.walk())
 print(Sheep1.eat())
 print(Sheep1.trim())
@@ -107,7 +98,7 @@ print()
 Sheep2 = Sheeps("Кудрявый", 78)
 animals_dict[Sheep2.name] = Sheep2.weight
 print(Sheep2.name)
-print(Sheep2.sound)
+print(Sheep2.voice())
 print(Sheep2.walk())
 print(Sheep2.eat())
 print(Sheep2.trim())
@@ -116,7 +107,7 @@ print()
 Hen1 = Hens("Ко-Ко", 1.5)
 animals_dict[Hen1.name] = Hen1.weight
 print(Hen1.name)
-print(Hen1.sound)
+print(Hen1.voice())
 print(Hen1.walk())
 print(Hen1.eat())
 print(Hen1.lay_eggs())
@@ -125,7 +116,7 @@ print()
 Hen2 = Hens("Кукареку", 2)
 animals_dict[Hen2.name] = Hen2.weight
 print(Hen2.name)
-print(Hen2.sound)
+print(Hen2.voice())
 print(Hen2.walk())
 print(Hen2.eat())
 print(Hen2.lay_eggs())
@@ -134,7 +125,7 @@ print()
 Goat1 = Goats("Рога", 50)
 animals_dict[Goat1.name] = Goat1.weight
 print(Goat1.name)
-print(Goat1.sound)
+print(Goat1.voice())
 print(Goat1.walk())
 print(Goat1.eat())
 print(Goat1.get_milk())
@@ -143,7 +134,7 @@ print()
 Goat2 = Goats("Копыта", 60)
 animals_dict[Goat2.name] = Goat2.weight
 print(Goat2.name)
-print(Goat2.sound)
+print(Goat2.voice())
 print(Goat2.walk())
 print(Goat2.eat())
 print(Goat2.get_milk())
@@ -152,7 +143,7 @@ print()
 Duck1 = Ducks("Кряква", 3)
 animals_dict[Duck1.name] = Duck1.weight
 print(Duck1.name)
-print(Duck1.sound)
+print(Duck1.voice())
 print(Duck1.walk())
 print(Duck1.eat())
 print(Duck1.lay_eggs())
